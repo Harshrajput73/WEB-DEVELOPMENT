054d0d2d6f65647a7412119e8a4fd1a862a15a26
Harshrajput73 <hraj6650@gmail.com>
2024-01-21 18:07:34 +0530

    learn javascript variable

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,9 @@
-
-const name="harsh raj singh";
+const student= {
+    fullName: "harsh raj singh",
+    age:23,
+    cgpa:7.35,
+    ispass:true,
+};
+student ["age"]=student["age"] +1
+student ["Name"]="Harsh solanki"
+console.log(student.Name);
