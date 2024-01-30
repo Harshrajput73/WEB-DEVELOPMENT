8cd1597773266088d6922cacbc18be0094371fc2
Harshrajput73 <hraj6650@gmail.com>
2024-01-30 23:31:35 +0530

    design a web page

--- javascript/practice.js
+++ javascript/practice.js
@@ -5,17 +5,30 @@
 //     working:"enterpenuer",
 // };
 // console.log(profile);
-let score=75;
-let grade;
-if(score >=90 && score <=100){
-    grade ="A";
-} else if (score >=70 && score <=89){
-    grade ="B";
-} else if (score >=60 && score <=69){
-    grade ="C";
-} else if (score >=50 && score <=59){
-    grade ="D";
-} else if (scor >=0 && score <=49){
-    grade ="F";
-} 
-console .log ("according to your scores , your grade was :", grade)
+// let score=75;
+// let grade;
+// if(score >=90 && score <=100){
+//     grade ="A";
+// } else if (score >=70 && score <=89){
+//     grade ="B";
+// } else if (score >=60 && score <=69){
+//     grade ="C";
+// } else if (score >=50 && score <=59){
+//     grade ="D";
+// } else if (scor >=0 && score <=49){
+//     grade ="F";
+// } 
+// console .log ("according to your scores , your grade was :", grade)
+// let sum=0;
+// let n=5;
+// for(let i=1; i<=n; i++){
+//     sum=sum +i;
+// }
+console.log("sum is ",sum);
+// let n=100;
+// for(let i=1; i<=n; i++){
+//     if (i%2==0){
+//         console.log("i= +",i);  
+//     }
+    
+// }
