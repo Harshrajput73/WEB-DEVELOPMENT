dd5de62e230dbbca4e7f1e7d63e279bae4a48816
Harshrajput73 <hraj6650@gmail.com>
2024-02-22 23:37:56 +0530

    create a analog watch and digital watch

---
+++ javascript/dom1.js
@@ -0,0 +1,12 @@
+// const rootNode =  document.getRootNode();
+// const htmlElementNode = rootNode.childNodes[0];
+// // console.log(htmlElementNode.childNodes);
+// const headElementNode =htmlElementNode.childNodes[0];
+// const textNode1 = htmlElementNode.childNodes[1];
+// const bodyElementNode = htmlElementNode.childNodes[2];
+// console.log(textNode1);
+const h1= document.querySelector("h1");
+const body=h1.parentNode.parentNode;
+body.style.color="#efefef";
+body.style.backgroundColor="#333";
+
